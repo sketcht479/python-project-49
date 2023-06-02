9cb6ea43bcd9890e40e3ddee1825407a927f481b
Анатолий Мелещенко WPC <sketch.t479@gmail.com>
2023-06-02 18:17:47 +0300

    linter brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,28 +2,28 @@ import prompt
 from random import randint
 from brain_games.cli import welcome_user
 
+
 def correct(number):
     if number % 2 == 0:
         return 'yes'
     else:
         return 'no'
 
+
 def main():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no". ')
     counter = 0
-    while counter <3:
+    while counter < 3:
         num = randint(0, 100)
-        answer = prompt.string('Question:'+ str(num) + ' ')
+        answer = prompt.string('Question:' + str(num) + ' ')
         if answer == correct(num):
             print('Correct!')
             counter += 1
         else:
             print("'"+answer + "'" + " is wrong answer ;(. Correct answer was " + "'" + correct(num)+"'")
             break
-    print('Congratulations '+ name)
-
-
+    print('Congratulations ' + name)
 
 
 if __name__ == '__main__':
